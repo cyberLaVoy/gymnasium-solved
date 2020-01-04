4feae85c54d22e1b47b37b0c2475f5abb6365457
CyberLaVoy <brentenlavoy@gmail.com>
2020-01-03 18:52:02 -0700

    Atari problem start, updated Mountain Car

---
+++ atari/breakoutAI.py
@@ -0,0 +1,17 @@
+import gym, time
+
+env = gym.make("Breakout-v0")
+observation = env.reset()
+
+for _ in range(1000000):
+  env.render()
+
+  action = env.action_space.sample() # your agent here (this takes random actions)
+
+  observation, reward, done, info = env.step(action)
+  if reward != 0:
+      print("reward", reward)
+
+  if done:
+    observation = env.reset()
+env.close()
--- mountain-car/mountainCarAI.py
+++ mountain-car/mountainCarAI.py
@@ -1,122 +1,94 @@
 #!/usr/bin/env python3
-import gym, joblib, sys
+import gym, joblib
 import numpy as np 
 
-NUM_DISCRETE_SECTIONS = 500
+# this number is how many sections each coninuous observation varibable is split into
+# note: since the environment isn't complex, performance is best when number is low (but not too low)
+NUM_DISCRETE_SECTIONS = 25
 
 def saveModel(model, modelPath):
     joblib.dump(model, modelPath) 
 def loadModel(modelPath):
     return joblib.load(modelPath)
 
-def learnQTable(env, Q, completionTries=100, alpha=.5, gamma=.5, epochs=5000, randomInfluence=1, visuaDisplay=False):
-    rev_list = [] # rewards per epochs calculate
-    maxPosition = -.45
-    # Q-learning Algorithm
-    for i in range(epochs):
+def observeAgent(env, Q):
+    while True:
         s = getDiscreteObservation(env, env.reset()) 
-        rAll = 0
-        done = False
-        tries = 0 
-        # The Q-Table learning algorithm
-        while tries < completionTries:
-            tries += 1
-            # Choose action from Q table; influence of randomness decreases as i increases
-            a = np.argmax(Q[s,:] + np.random.randn(1,env.action_space.n)*(randomInfluence/(i+1))) 
-            #Get new state & reward from environment
-            s1,reward,done,_ = env.step(a)
-            position = s1[0]
-            if position >= env.goal_position: 
-                #print("Yay - I made it!!")
-                reward = 1
-            if position > maxPosition:
-                reward += .5
-                maxPosition = position
-            if abs(position+.5) > 0:
-                reward += .15
-            if s1[1] < 0 and a == 0:
-                reward += .25
-            if s1[1] > 0 and a == 2:
-                reward += .25
-            
-            s1 = getDiscreteObservation(env, s1)
-            #Update Q-Table with new knowledge
-            Q[s,a] = (1-alpha)*Q[s,a] + alpha*(reward + gamma*np.max(Q[s1,:]))
-            rAll += reward
-            s = s1
-            if done and position >= env.goal_position:
+        while True:
+            a = np.argmax(Q[s,:]) 
+            s,_,_,_ = env.step(a)
+            if s[0] >= env.goal_position:
+                print("Made it!")
                 break
-            if visuaDisplay:
+            s = getDiscreteObservation(env, s) 
+            env.render()
+
+
+def learnQTable(env, Q, periods=250, alpha=.7, gamma=.8, epochs=5000, decay=.99, render=False):
+    randomInfluence = 1
+    almost = False
+    goalCount = 0
+    for epoch in range(1, epochs+1):
+        if epoch % 100 == 0:
+            print("Epoch", str(epoch)+'/'+str(epochs), "Acheivement", str(goalCount)+'/'+"100")
+            goalCount = 0
+        s = getDiscreteObservation(env, env.reset()) 
+        for _ in range(periods):
+            if render:
                 env.render()
-        rev_list.append(rAll)
-    print( "Reward Sum on all epochs " + str(sum(rev_list)/epochs) )
-    print( "Final Values Q-Table" )
-    #np.set_printoptions(threshold=sys.maxsize)
-    print( Q )
-    print( Q.shape )
+            # Choose action from Q table; influence of randomness decreases iterations
+            randomInfluence *= decay
+            a = np.argmax(Q[s,:] + np.random.randn(1,env.action_space.n)*randomInfluence) 
+            # get new state and reward from environment, after taking action
+            s1,reward,_,_ = env.step(a)
+            # reward engineering
+            reward = -1
+            if s1[0] >= env.goal_position*2/3 and not almost:
+                reward = .25 # a one-time reward to help the agent know what to shoot for
+                almost = True
+            if s1[0] >= env.goal_position:
+                goalCount += 1
+                reward = 1
+            s1 = getDiscreteObservation(env, s1)
+            # update Q-Table with new knowledge
+            Q[s,a] = (1-alpha)*Q[s,a] + alpha*(reward + gamma*np.max(Q[s1,:]))
+            s = s1
+            if reward == 1:
+                break
 
-def evaluateQTable(env, Q, numTests, visuaDisplay=False):
-    totalOfRewards = 0
-    totalCompleteRuns = 0
-    totalIncompleteRuns = 0
-    for i in range(numTests):
-        observation = env.reset()
-        position = observation[0]
-        observation = getDiscreteObservation(env, observation)
-        totalReward = 0 
-        done = False
-        while not done:
-            action = np.argmax(Q[observation,:])
-            observation, reward, done, _ = env.step(action)
-            position = observation[0]
-            observation = getDiscreteObservation(env, observation)
-            totalReward += reward
-            if visuaDisplay:
-                env.render()
-        if done and position >= env.goal_position:
-            totalCompleteRuns += 1
-            totalOfRewards += totalReward
-        else:
-            totalIncompleteRuns += 1
-    print("Total complete runs:", totalCompleteRuns)
-    print("Total incomplete runs:", totalIncompleteRuns)
-    print("Average steps to complete:", totalOfRewards/totalCompleteRuns)
 
-def getNumDiscretePositions(env, numSections):
-    sizeOfPositionRange = env.observation_space.high[0] - env.observation_space.low[0]
-    return sizeOfPositionRange // ( sizeOfPositionRange / numSections )
-def getNumDiscreteSpeeds(env, numSections):
-    sizeOfSpeedRange = env.observation_space.high[1] - env.observation_space.low[1]
-    return sizeOfSpeedRange // ( sizeOfSpeedRange / numSections )
-
+# provides a unique integer for any given pair of positive integers
+def cantorPairing(a, b):
+    return int( 1/2*(a+b)*(a+b+1)+b )
 def getDiscreteObservation(env, observation, numSections=NUM_DISCRETE_SECTIONS):
-    discretePosition =  ( observation[0] + abs(env.observation_space.low[0]) ) * numSections 
-    discreteSpeed =  ( observation[1] + abs(env.observation_space.low[1]) ) * numSections 
-    return int( discreteSpeed*getNumDiscretePositions(env, numSections) + discretePosition )
+    positionRange = abs(env.observation_space.high[0]) + abs(env.observation_space.low[0])
+    normalizedPosition = (observation[0] + abs(env.observation_space.low[0]) ) / positionRange # a value between 0 and 1
+    discretePosition = int( normalizedPosition * numSections )
+    speedRange = abs(env.observation_space.high[1]) + abs(env.observation_space.low[1])
+    normalizedSpeed = (observation[1] + abs(env.observation_space.low[1]) ) / speedRange # a value between 0 and 1
+    discreteSpeed = int( normalizedSpeed * numSections )
+    return cantorPairing(discretePosition, discreteSpeed)
+def getNumDiscreteObservations(numDiscreteSections=NUM_DISCRETE_SECTIONS):
+    return cantorPairing(numDiscreteSections, numDiscreteSections)
 
-def getNumDiscreteObservations(env, numSections=NUM_DISCRETE_SECTIONS):
-    return getDiscreteObservation(env, [env.observation_space.high[0], env.observation_space.high[1]], numSections)
 
 def main():
-    QTableName = "mouatingCarAI_Q-table"
+    QTableName = "MountainCarAI-Qtable"
     env = gym.make('MountainCar-v0')
-    learnAndSave = True
-    loadAndEvaluate = False
+
+    learnAndSave = False
+    loadAndObserve = True
 
     if learnAndSave:
         # env.observation_space.low = [min_position, min_speed]
         # env.observation_space.high = [max_position, max_speed]
-        #numDiscreteObservations = getNumDiscreteObservations(env)
-        #Q = np.zeros( [ numDiscreteObservations, env.action_space.n ] )
-        Q = loadModel(QTableName+".joblib")
-        #learnQTable(env, Q, completionTries=300, alpha=.65, gamma=.8, epochs=240000, randomInfluence=3, visuaDisplay=False)
-        learnQTable(env, Q, completionTries=300, alpha=.5, gamma=.7, epochs=5000, randomInfluence=0, visuaDisplay=True)
+        Q = np.zeros( [getNumDiscreteObservations(), env.action_space.n] )
+        learnQTable(env, Q, render=False)
         saveModel(Q, QTableName+".joblib")
 
-    if loadAndEvaluate:
+    if loadAndObserve:
         Q = loadModel(QTableName+".joblib")
-        evaluateQTable(env, Q, numTests=15000, visuaDisplay=False)
-
+        observeAgent(env, Q)
 
 if __name__ == "__main__":
     main()
